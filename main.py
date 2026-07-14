6295493ff826503021eada2bfada5d527a2f54f9
MAyorla <batmaral1@gmail.com>
2026-07-14 20:37:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, GitHub!")
+import random
+
+print("🎲 Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+# Random number
+secret_number = random.randint(1, 100)
+
+while True:
+    # User input
+    guess = int(input("Enter your guess: "))
+
+    if guess < secret_number:
+        print("Too low! Try again.\n")
+
+    elif guess > secret_number:
+        print("Too high! Try again.\n")
+
+    else:
+        print(f"🎉 Congratulations! You guessed the number: {secret_number}")
+        break
